45db5e858c7e606267ca367363a5cd55c6036056
arunpriya <arunpriya.dharmaraj@gmail.com>
2017-08-05 13:19:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML="new value";
 
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveLeft()
+{
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft=marginLeft + 'px';
+}
 img.onclick=function(){
     var interval=setInterval(moveLeft,100);
 };
