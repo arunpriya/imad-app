2f188f2728dfec577538707ea07d556e00e0b13b
arunpriya <arunpriya.dharmaraj@gmail.com>
2017-08-21 22:55:17 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,24 @@ app.post('/create-user',function(req,res){
     }); 
 });
 
+app.post('/login',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    pool.query('SELECT * FROM "user_db" WHERE username = $1)',[username],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0)
+            {
+                res.status(403).send('username/password is invalid!!!');
+            }else{
+                res.send('User successfully created:'+username);
+            }
+        }
+    }); 
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
