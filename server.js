b360b13ac57f342f1ebf571c0f84e71a3cafef81
arunpriya <arunpriya.dharmaraj@gmail.com>
2017-08-21 19:04:16 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/submit-name',function(req,res){
 
 app.get('/articles/:articleName',function(req,res){
     var articleName=req.params.articleName;
-    pool.query("SELECT * FROM articles WHERE title= $1",[articleName],function(err,result){
+    pool.query("SELECT * FROM articles_db WHERE title= $1",[articleName],function(err,result){
        if(err){
             res.status(500).send(err.toString());
         }else{
