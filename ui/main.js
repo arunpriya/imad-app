276d96615baa4a6561171204a773bccac9e4cc61
arunpriya <arunpriya.dharmaraj@gmail.com>
2017-08-21 23:24:20 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,6 @@ submit.onclick = function(){
     console.log(username);
     console.log(password);
     request.open('POST','http://arunpriyadharmaraj73.imad.hasura-app.io/login',true);
-    request.setRequestHeader('Content-Type':'application/json');
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
 };
