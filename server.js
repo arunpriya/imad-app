bcfdc8bf6c59ded751203ce1ddd2ce5882b640d8
arunpriya <arunpriya.dharmaraj@gmail.com>
2017-08-21 22:19:17 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
-
+var bodyParser = require('body-parser');
 
 var config={
     user:'arunpriyadharmaraj73',
@@ -15,6 +15,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use();
 
 function createTemplate(data){
     var title=data.title;
@@ -81,7 +82,7 @@ app.post('/create-user',function(req,res){
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
     var dbString=hash(password,salt);
-   pool.query('SELECT * FROM test',function(err,result){
+   pool.query('INSERT INTO "user_db" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
